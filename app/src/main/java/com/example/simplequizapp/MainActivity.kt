22a660ffbc133b3520dfe5dcb43b304d7a2d5395
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-05-16 07:28:22 +0300

    add viewBinding

--- app/src/main/java/com/example/simplequizapp/MainActivity.kt
+++ app/src/main/java/com/example/simplequizapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.simplequizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simplequizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
